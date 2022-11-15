1321b0497fd5f101d37f45576a33512eeea9fe20
derPatecopa <mb89@me.com>
2022-11-15 12:05:42 +0100

    finished code

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-const collectiveWisdom = {
-    signInfo: ['star', 'moon', 'sun', 'comet'],
-    fortuneOutput: ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
-    advice: ['go out and eat', 'not read this', 'play more', 'trust no one']
+const collectiveWisdom = [['star', 'moon', 'sun', 'comet'],
+    ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
+    ['go out and eat', 'not read this', 'play more', 'trust no one']]
+
+const wisdomMessage = (arr) => {
+    let messageArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let randomNumber = Math.floor(Math.random()*arr[i].length);
+        messageArr.push(arr[i][randomNumber]);
+    } return messageArr;
 }
 
-let signInfo = "Your sign right now is a";
-let fortuneOutput = "You are having:";
-let advice = "You should";
+const showMessage = arr => {
+   console.log(`Your sign right now is a ${arr[0]}`);
+   console.log(`You are having ${arr[1]}`);
+   console.log(`You should ${arr[2]}`);
+}
 
-
+showMessage(wisdomMessage(collectiveWisdom));
